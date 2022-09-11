54f9d82330033de9d4344b9e5db22542d38294bc
Chanwoo Park <99chanwoo@gmail.com>
2022-09-11 16:19:28 +0900

    feat: declare constants

---
+++ src/token/token.go
@@ -0,0 +1,35 @@
+package toekn
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifier + Literal
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operator
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Separator
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	// Parantheseses
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Reserved
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
