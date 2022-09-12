4371deec4ef2ba5585c5c9046fca2c999635ce81
Chanwoo Park <99chanwoo@gmail.com>
2022-09-12 14:19:38 +0900

    refactor: use internal function for repetitive assertion

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -10,6 +10,18 @@ type TokenTestcase struct {
 	expectedLiteral string
 }
 
+func evaulateTestcases(lex *Lexer, tcs []TokenTestcase, t *testing.T) {
+	for i, tt := range tcs {
+		tok := lex.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
 func TestNextTokenSpecialCharacters(t *testing.T) {
 	input := `=+(){},;`
 
@@ -26,16 +38,7 @@ func TestNextTokenSpecialCharacters(t *testing.T) {
 	}
 
 	l := New(input)
-
-	for i, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
-		}
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
-		}
-	}
+	evaulateTestcases(l, tests, t)
 }
 
 func TestNextTokenRealworldCase1(t *testing.T) {
@@ -88,14 +91,5 @@ func TestNextTokenRealworldCase1(t *testing.T) {
 	}
 
 	l := New(input)
-
-	for i, tt := range tests {
-		tok := l.NextToken()
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
-		}
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
-		}
-	}
+	evaulateTestcases(l, tests, t)
 }
