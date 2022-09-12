ec59583c2af9972f8718b539220e9035f2ae8cda
Chanwoo Park <99chanwoo@gmail.com>
2022-09-13 00:15:28 +0900

    feat: add parser primitive

---
+++ src/monkey/parser/parser.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	lex       *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(lex *lexer.Lexer) *Parser {
+	ps := &Parser{lex: lex}
+	ps.nextToken()
+	ps.nextToken()
+
+	return ps
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lex.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
