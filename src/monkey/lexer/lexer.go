b2b954d24a1ac994aa999f13dd42bbe65aad2e1a
Chanwoo Park <99chanwoo@gmail.com>
2022-09-12 14:53:49 +0900

    feat: add peekChar method

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -15,16 +15,6 @@ func New(input string) *Lexer {
 	return l
 }
 
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition += 1
-}
-
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
@@ -45,19 +35,19 @@ func (l *Lexer) NextToken() token.Token {
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
 
-	// Comparator
+		// Comparator
 	case '<':
 		tok = newToken(token.LT, l.ch)
 	case '>':
 		tok = newToken(token.GT, l.ch)
 
-	// Separators
+		// Separators
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 
-	// Parantheses
+		// Parantheses
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
@@ -67,7 +57,7 @@ func (l *Lexer) NextToken() token.Token {
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
 
-	// Error check
+		// Error check
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -121,4 +111,22 @@ func isDigit(ch byte) bool {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
