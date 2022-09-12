790fa717b7fcf503ef4d1c56d84ef17786ae278a
Chanwoo Park <99chanwoo@gmail.com>
2022-09-12 13:58:19 +0900

    feat: add identifier related methods

--- src/monkey/lexer/lexer.go
+++ src/monkey/lexer/lexer.go
@@ -3,10 +3,10 @@ package lexer
 import "monkey/token"
 
 type Lexer struct {
-	input string 
-	position int // Current cursor
-	readPosition int // Next character to be searched
- 	ch byte // Character currently evaluated
+	input        string
+	position     int  // Current cursor
+	readPosition int  // Next character to be searched
+	ch           byte // Character currently evaluated
 }
 
 func New(input string) *Lexer {
@@ -48,10 +48,29 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' < ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
