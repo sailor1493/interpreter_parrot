9327cb1b5efaa2c09fed61f5bfeae9ced0234741
Chanwoo Park <99chanwoo@gmail.com>
2022-09-14 08:27:52 +0900

    feat: implement parseReturnStatement

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -47,6 +47,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -69,6 +71,15 @@ func (p *Parser) parseLetStatement() ast.Statement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() ast.Statement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
 	return stmt
 }
 
