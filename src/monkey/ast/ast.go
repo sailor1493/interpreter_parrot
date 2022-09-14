f2d7673eaf236ff2a397168fe9d9e304fee05068
Chanwoo Park <99chanwoo@gmail.com>
2022-09-14 09:36:53 +0900

    feat: define IntegerLiteral

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -90,6 +90,15 @@ func (i *Identifier) expresssionNode()     {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
 // Program and Program builder Section
 
 type Program struct {
